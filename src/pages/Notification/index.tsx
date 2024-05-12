e9bffef91d3140e5d3965158a3e6b4357dbd1eda
tan <tanat8511@gmail.com>
2024-05-12 20:32:22 +0500

    feat: additional pages

---
+++ src/pages/Notification/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TextSubtext from "../../components/textSubtext";
+
+const Notification = () => {
+    return (
+        <>
+            <TextSubtext text={'Notifications'} subText={'From users and other'}/>
+            <section>
+                coming soon...
+            </section>
+        </>
+    );
+};
+
+export default Notification;
---
+++ src/pages/Support/Support.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TextSubtext from "../../components/textSubtext";
+
+const Support = () => {
+    return (
+        <>
+            <TextSubtext text={'Support information'} subText={'write your problems'}/>
+            <section>
+                coming soon...
+            </section>
+        </>
+    );
+};
+
+export default Support;
